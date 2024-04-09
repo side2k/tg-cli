6cbc9514746a6ea77345d2467ca1d97a9828e520
Leonid Amirov <leonid.amirov@gmail.com>
2024-04-09 23:04:21 +0300

    refactor: make session and client vars shared between command arms

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,20 @@ use clap::Parser;
 #[tokio::main]
 async fn main() {
     let cli_args = cli::Cli::parse();
+
+    let session = telegram::get_session(cli_args.session_file.clone());
+    let client = telegram::get_client(cli_args.api_id, cli_args.api_hash, session).await;
+
     match cli_args.command {
         cli::Commands::Login { phone, password } => {
             println!("Logging in {}", phone);
-            let session = telegram::get_session(cli_args.session_file.clone());
-            let client = telegram::get_client(cli_args.api_id, cli_args.api_hash, session).await;
             let me = telegram::login(&client, phone, password).await.unwrap();
             println!("Logged in as '{}'", me.full_name());
-            client
-                .session()
-                .save_to_file(cli_args.session_file)
-                .unwrap();
         }
     }
+
+    client
+        .session()
+        .save_to_file(cli_args.session_file)
+        .unwrap();
 }
